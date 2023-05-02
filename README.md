77a7860e2c2506fcfa54da78d86cd674063bcda7
Breeannr180 <breeannr@hotmail.com>
2023-05-02 13:53:34 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # svg-logo-maker
+
+
+This projeect was created to assist a user in creating and generating a logo.
+
+
+## Installation
+
+User will need to open their terminal and run node index.js
+
+## Usage
+
+![SVGLogo](./images/Screenshot%20of%20logo.png)
+
+https://github.com/Breeannr180/svg-logo-maker
+
+## License
+
+NA
+
--- libs/shapes.test.js
+++ libs/shapes.test.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
+const { square } = require('./shapes');
 
 describe('Logo generator', () => {
   it('generates a circle logo', () => {
     // Your code to generate a circle logo goes here
-    // ...
+    const shape = new Circle();
+    shape.setColor("blue");
+    expect(shape.render()).toEqual('<circle cx="150" cy="100" r="50" fill="blue" />');
 
     // Check if the logo file exists
     expect(fs.existsSync('logo.svg')).toBeTruthy();
@@ -19,7 +22,9 @@ describe('Logo generator', () => {
 
   it('generates a square logo', () => {
     // Your code to generate a square logo goes here
-    // ...
+    const shape = new Square();
+    shape.setColor("blue");
+    expect(shape.render()).toEqual('<rect x="100" y="50" width="100" height="100" fill="blue" />');
 
     // Check if the logo file exists
     expect(fs.existsSync('logo.svg')).toBeTruthy();
@@ -37,7 +42,7 @@ describe('Logo generator', () => {
     // Your code to generate a triangle logo goes here
     const shape = new Triangle();
     shape.setColor("blue");
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+    expect(shape.render()).toEqual('<polygon points="1150,50 50,150 250,150" fill="blue" />');
 
     // Check if the logo file exists
     expect(fs.existsSync('logo.svg')).toBeTruthy();
